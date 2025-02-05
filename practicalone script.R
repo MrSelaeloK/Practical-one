74f26ff8bf8346df14894523212896ceb40aa6d2
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-05 14:40:57 +0200

    headings of the new data frame set

--- practicalone script.R
+++ practicalone script.R
@@ -7,4 +7,8 @@ number_of_columns<-ncol(airquality)
 number_of_columns
 
 #---data frame with size = to number rows with missing values---#
-mvalues_df <-data.frame()
+# names of headings also set to match #
+airquality_matrix<-as.matrix(airquality)
+missingValues_df <-data.frame(matrix(NA,nrow=number_of_missing_cases, ncol=number_of_columns))
+colnames(missingValues_df)<-colnames(airquality_matrix)
+missingValues_df
