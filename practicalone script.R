2f930a46330dec682c128336c139747d701ac071
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-05 14:20:21 +0200

    number of columns and rows required for a dataframe made only of the missing row data

--- practicalone script.R
+++ practicalone script.R
@@ -1,3 +1,10 @@
 ###Practical one ####
 airquality
-x<-1
+#---number of missing data rows and number of total columns in the air quality database---#
+number_of_missing_cases <- sum(!complete.cases(airquality))
+number_of_missing_cases
+number_of_columns<-ncol(airquality)
+number_of_columns
+
+#---data frame with size = to number rows with missing values---#
+mvalues_df <-data.frame()
