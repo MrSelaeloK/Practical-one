896cfc4e71a87f6e650fedd888d77ec4893c86f1
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-06 17:55:25 +0200

    final update to the Practical

--- practicalone script.R
+++ practicalone script.R
@@ -2,9 +2,8 @@
 airquality
 #---number of missing data rows and number of total columns in the air quality database---#
 number_of_missing_cases <- sum(!complete.cases(airquality))
-number_of_missing_cases
 number_of_columns<-ncol(airquality)
-number_of_columns
+print(paste("missing values data frame will have ",number_of_missing_cases," rows and ",number_of_columns," columns"))
 
 #---data frame with size = to number rows with missing values---#
 # names of headings also set to match #
@@ -28,27 +27,58 @@ missingValues_df
 
 #---mean, min, standard deviation, max for ozone and temperature---#
 
-Ozone_mean <- mean(airquality[,1],,na.rm=TRUE)
-Ozone_sd<-sd(airquality[,1],na.rm=TRUE)
-Ozone_min <- min(airquality[,1],na.rm=TRUE)
-Ozone_max <- max(airquality[,1],na.rm=TRUE)
+Ozone_mean <- mean(airquality[,1],
+                   ,
+                   na.rm=TRUE)
 
-Temp_mean <- mean(airquality[,4],na.rm=TRUE)
-Temp_sd <- sd(airquality[,4],na.rm=TRUE)
-Temp_min <- min(airquality[,4],na.rm=TRUE)
-Temp_max <- max(airquality[,4],na.rm=TRUE)
+Ozone_sd<-sd(airquality[,1],
+             na.rm=TRUE)
+
+Ozone_min <- min(airquality[,1],
+                 na.rm=TRUE)
+
+Ozone_max <- max(airquality[,1],
+                 na.rm=TRUE)
+
+Temp_mean <- mean(airquality[,4],
+                  na.rm=TRUE)
+
+Temp_sd <- sd(airquality[,4],
+              na.rm=TRUE)
+
+Temp_min <- min(airquality[,4],
+                na.rm=TRUE)
+
+Temp_max <- max(airquality[,4],
+                na.rm=TRUE)
+
+ozone_output<- paste("Ozone mean: ",Ozone_mean,
+                     "\nOzone standard deviation: ",Ozone_sd,
+                     "\nOzone max: ",Ozone_max,
+                     "\nOzone min: ", Ozone_min)
+
+temperature_output<-paste("Temperature mean: ",Temp_mean,
+                          "\nTemperature standard deviation: ",Temp_sd,
+                          "\nTemperature max: ",Temp_max,
+                          "\nTemperature min: ",Temp_min)
+
+cat(paste0(ozone_output,"\n\n",temperature_output))
 
 #---Question 3 ---#
-intercept_vector<-rep(1,nrow(cars))
-
-X<-as.matrix(cbind(intercept_vector,cars[1]))
+intercept<-rep(1,nrow(cars))
+X<-as.matrix(cbind(intercept,(cars[,1])))
 Y<-as.matrix(cars[,2])
 
-
-#beta_coefficients <- ((t(X)%*%X)^-1) %*% t(X) %*% Y
-beta_coefficients <- solve(t(X)%*%X) %*% (t(X)%*%Y)
-
-beta_coefficients
+f<-function(X,Y){
+  beta_coefficients <- solve(t(X)%*%X) %*% (t(X)%*%Y)
+  colnames(beta_coefficients)<-"Coefficients"
+  return(list(beta_coefficients))
+}
+f(X,Y)
 #---Question 4 ---#
-cars
-lm(cars[,2]~cars[,1])
+a<-lm(cars[,2]~cars[,1])
+summary(a)
+
+
+
+
